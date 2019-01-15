26d2aa36b2314d75b8a589ce2ad7f37c628bebd9
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2019-01-15 13:53:28 +0100

    nixpkgs path

---
+++ bash.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  programs.bash.shellAliases = {
+    gs = "git status";
+    gct = "git commit";
+    gad = "git add";
+    scrsnip = "scrot -s ~/screenshots/%b%d::%H%M%S.png";
+    scr = "scrot ~/screenshots/%b%d::%H%M%S.png";
+    npp = "nix-instantiate --eval --expr \"<nixpkgs>\"";
+  };
+}
--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./xserver.nix
+      ./bash.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
